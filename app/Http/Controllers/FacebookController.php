d565c19cca4d6bad18f22569ef7b302279d79252
Andrew <hello@rapliandras.hu>
2015-01-05 19:58:32 +0100

    controller update

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -8,34 +8,29 @@ use Facebook\GraphUser;
 use Facebook\FacebookRequestException;
 use Facebook\FacebookJavaScriptLoginHelper;
 use RapliAndras\Facebook\Laravel\FacebookRedirectLoginHelper;
-use \Request;
-use \Redirect;
-use \Response;
 
 class FacebookController extends Controller{
 
-
-	public function login(){
-
+	public function login()
+	{
 		// Replace 1234 with your APP_ID
 		// Replace 123456789 with your APP_SECRET
 
 		FacebookSession::setDefaultApplication('1234', '123456789');
 
-		$redirect_url = Request::url();
-
-		$helper = new FacebookRedirectLoginHelper($redirect_url);
+		$helper = new FacebookRedirectLoginHelper(\Request::url());
 
 		if ($session = $helper->getSessionFromRedirect())
 		{
 		    $request = new FacebookRequest($session, 'GET', '/me');
 
-		    $user = $request->execute()->getGraphObject(GraphUser::className());
+		    $userData = $request->execute()->getGraphObject(GraphUser::className());
 
-		    return Response::make('You have arrived, '.$user->getName());
+		    $message = 'You have arrived, '.$userData->getName();
+		    return \Response::make($message);
 		}
 
-		return Redirect::to($helper->getLoginUrl());
+		return redirect($helper->getLoginUrl());
 
 	}
 
